97fc940abd81dd6e5cb9d38e3f9745073f8fe1de
ubuntu <itaizy@163.com>
2020-03-12 00:36:50 +0800

    保证提供图片

--- src/ImgCrawler.py
+++ src/ImgCrawler.py
@@ -13,23 +13,26 @@ options.add_argument('--window-size=343,768')
 driver = webdriver.Chrome(chrome_options=options)
 driver.get('http://ring.act.buaa.edu.cn/gb/')
 
+time.sleep(8)
+
 #下面的js代码根据canvas文档说明而来
-JS = 'return arguments[0].toDataURL("image/png", 1);'
-WidthJS = 'arguments[0].style.width="882px";'
-HeightJS = 'arguments[0].style.height="480px";'
+JS = 'return arguments[0].toDataURL("image/png", 1.0);'
+WidthJS = 'arguments[0].style.width="441px";'
+HeightJS = 'arguments[0].style.height="240px";'
 # 执行 JS 代码并拿到图片 base64 数据
 i = 0
-for img in driver.find_elements_by_xpath("//div[@id='Trends']//div[@class='echarts-for-react ']"):
-    driver.execute_script(WidthJS, img)  #执行js文件得到带图片信息的图片数据
-    driver.execute_script(HeightJS, img)  #执行js文件得到带图片信息的图片数据
-time.sleep(5)
+#for img in driver.find_elements_by_xpath("//div[@id='Trends']//div[@class='echarts-for-react ']"):
+#    driver.execute_script(WidthJS, img)  #执行js文件得到带图片信息的图片数据
+#    driver.execute_script(HeightJS, img)  #执行js文件得到带图片信息的图片数据
+#time.sleep(5)
 
 fnquanguo = []
 fnhubei = []
 
-for img in driver.find_elements_by_xpath("//div[@id='Trends']/div[1]//canvas"):
+for img in driver.find_elements_by_xpath("//div[@id='Trends']/div[1]//img"):
     tag = "images/quanguo"
-    im_info = driver.execute_script(JS, img)  #执行js文件得到带图片信息的图片数据
+    #im_info = driver.execute_script(JS, img)  #执行js文件得到带图片信息的图片数据
+    im_info = img.get_attribute("src")
     im_base64 = im_info.split(',')[1]  #拿到base64编码的图片信息
     im_bytes = base64.b64decode(im_base64)  #转为bytes类型
     with open(tag + "-" + str(i) + '.png','wb') as f:  #保存图片到本地
@@ -37,9 +40,10 @@ for img in driver.find_elements_by_xpath("//div[@id='Trends']/div[1]//canvas"):
     fnquanguo.append(tag + "-" + str(i) + '.png')
     i += 1
 i = 0
-for img in driver.find_elements_by_xpath("//div[@id='Trends']/div[2]//canvas"):
+for img in driver.find_elements_by_xpath("//div[@id='Trends']/div[2]//img"):
     tag = "images/hubei"
-    im_info = driver.execute_script(JS, img)  #执行js文件得到带图片信息的图片数据
+    #im_info = driver.execute_script(JS, img)  #执行js文件得到带图片信息的图片数据
+    im_info = img.get_attribute("src")
     im_base64 = im_info.split(',')[1]  #拿到base64编码的图片信息
     im_bytes = base64.b64decode(im_base64)  #转为bytes类型
     with open(tag + "-" + str(i) + '.png','wb') as f:  #保存图片到本地
